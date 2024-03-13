a526afa665db6a784f3f46a8f7cc2dece13166a7
Haroon Tahir <134407351+Ignavar@users.noreply.github.com>
2024-03-13 20:07:16 +0500

    Time: 6 ms (97.39%), Space: 20.4 MB (57.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,10 +3,11 @@ public:
     int missingNumber(vector<int>& nums) {
         int range = nums.size();
         int sum = 0;
-        for(int i =0;i<=range;i++)
-            sum+= i;
-        for(int i=0;i<range;i++)
+        for(int i=0;i<range;i++) {   
+            sum+=i;
             sum-=nums[i];
+        }
+        sum+=range;
         return sum;
     }
 };
